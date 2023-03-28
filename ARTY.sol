f3a46e04af455869f68ab0b866f48edd22dcec33
ArtyfactMetaverse <106493263+ArtyfactMetaverse@users.noreply.github.com>
2023-03-29 00:49:24 +0300

    Update ARTY.sol

--- ARTY.sol
+++ ARTY.sol
@@ -465,10 +465,10 @@ contract Artyfact is Context, IBEP20, Ownable {
     }
 
     function _burn(address account, uint256 amount) internal virtual {
-        require(account != address(0), 'ERC20: burn from the zero address');
+        require(account != address(0), 'BEP20: burn from the zero address');
 
         uint256 accountBalance = _balances[account];
-        require(accountBalance >= amount, 'ERC20: burn amount exceeds balance');
+        require(accountBalance >= amount, 'BEP20: burn amount exceeds balance');
         unchecked {
             _balances[account] = accountBalance - amount;
             // Overflow not possible: amount <= accountBalance <= totalSupply.
